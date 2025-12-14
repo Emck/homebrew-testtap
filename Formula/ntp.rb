bc086557f4344efa6a82f99b33ddd3ded5a210ed
Emck <emck.do@gmail.com>
2025-12-14 20:31:07 +0800

    copy ntp.rb from brew source

---
+++ Formula/ntp.rb
@@ -0,0 +1,40 @@
+class Ntp < Formula
+  desc "Network Time Protocol (NTP) Distribution"
+  homepage "https://www.ntp.org"
+  url "https://downloads.nwtime.org/ntp/4.2.8/ntp-4.2.8p18.tar.gz"
+  version "4.2.8p18"
+  sha256 "cf84c5f3fb1a295284942624d823fffa634144e096cfc4f9969ac98ef5f468e5"
+  license all_of: ["BSD-2-Clause", "NTP"]
+
+  livecheck do
+    url "https://downloads.nwtime.org/ntp/"
+    regex(/href=.*?ntp[._-]v?(\d+(?:\.\d+)+(?:p\d+)?)\.t/i)
+  end
+
+  no_autobump! because: :requires_manual_review
+
+  depends_on "openssl@3"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-openssl-libdir=#{Formula["openssl@3"].lib}
+      --with-openssl-incdir=#{Formula["openssl@3"].include}
+      --with-net-snmp-config=no
+    ]
+
+    system "./configure", *args
+    ldflags = "-lresolv"
+    ldflags = "#{ldflags} -undefined dynamic_lookup" if OS.mac?
+    system "make", "install", "LDADD_LIBNTP=#{ldflags}"
+  end
+
+  test do
+    # On Linux all binaries are installed in bin, while on macOS they are split between bin and sbin.
+    ntpdate_bin = OS.mac? ? sbin/"ntpdate" : bin/"ntpdate"
+    assert_match "step time server ", shell_output("#{ntpdate_bin} -bq pool.ntp.org")
+  end
+end
